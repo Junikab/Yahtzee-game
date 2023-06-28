757d21f2ee8d564fa2189c48a5ee3fbe75d19e12
Jenny <junikab@gmail.com>
2023-06-28 13:40:12 +1000

    115. replacing text with dice icon

--- yahtzee/src/Die.js
+++ yahtzee/src/Die.js
@@ -2,24 +2,23 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+    static defaultProps = {
+        numberWords: ["one", "two", "three", "four", "five", "six"],
+    };
     constructor(props) {
         super(props);
-        
+
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick() {
         this.props.handleClick(this.props.idx);
     }
     render() {
+        const { numberWords, val, locked, disabled } = this.props;
+        let classes = `Die fa fa-dice-${numberWords[val - 1]} fa-5x `;
+        if (locked) classes += "Die-locked";
         return (
-            <button
-                className="Die"
-                style={{
-                    backgroundColor: this.props.locked ? "grey" : "black",
-                }}
-                onClick={this.handleClick}            >
-                {this.props.val}
-            </button>
+        <i className={classes} onClick={this.handleClick} disabled={disabled}/>
         );
     }
 }
